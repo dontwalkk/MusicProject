23985686ea1aead78b894300010e7db7d19915a9
Robert K <robertkhachatryan452@gmail.com>
2024-09-04 20:54:34 -0700

    Added sign in pop up and page

--- js/index.js
+++ js/index.js
@@ -10,3 +10,27 @@ function changeTheme () {
 	}
   document.getElementById('nav-bar').style.backgroundColor='#565869'
 }
+
+const signInModal = document.getElementById('signInModal')
+const signInButton = document.getElementById('signInButton')
+const close = document.getElementById('close')
+const email = document.getElementById('email')
+const password = document.getElementById('password')
+const submit = document.getElementById('submit')
+
+signInButton.onclick = function() {
+  signInModal.style.display = 'block';
+}
+
+close.onclick = function() {
+  signInModal.style.display = 'none';
+}
+
+submit.addEventListener('click', () => {
+  const userEmail = email.value
+  alert(userEmail)
+  console.log(`E-mail: ${email.value}`);
+  const userPassword = password.value
+  alert(userPassword)
+  console.log('Password:', password.value);
+})
